5801ab3f832ed39c8147ed9f232b6faa71366ec9
GreenLoontern <gselzer@tds.net>
2016-11-04 22:00:01 -0500

    created class for Project 5

---
+++ src/SmallestMultiple.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author G
+ */
+public class SmallestMultiple {
+    /*
+    2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+    What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+    */
+    
+    public static void main(String[] args){
+        
+    }
+    
+}
